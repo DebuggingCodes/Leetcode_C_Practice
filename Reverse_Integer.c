f95d5c38f046d6581b7712e8fad40dae65ee2652
DebuggingCodes <goelmehul.08@gmail.com>
2025-11-20 23:19:25 +0530

    Create Reverse_Integer.c
    Had problem with making the code work for the designated limit
    https://leetcode.com/problems/reverse-integer/description/

---
+++ Reverse_Integer.c
@@ -0,0 +1,56 @@
+int reverse(int x){
+    int power=0, reverse=0,min=0;
+    if(x>INT_MAX/10||x<INT_MIN/10){
+        return 0;
+    }
+    if(x<0){
+        min=-x;
+    }
+    else{
+        min=x;
+    }
+    if(x==0){
+        return 0;
+    }
+    if(min%10==0){
+        while(min%10==0){
+            min=min/10;
+        }
+    }
+    while(min>pow(10,power)){
+        power++;
+    }
+    if (power==0&&x>0){
+        return min;
+    }
+    if (power==0&&x<0){
+        return -min;
+    }
+    //power done
+    int *solver= (int*)malloc(power * sizeof(int));
+    //dynamic array done
+    for(size_t i=0;i<power;i++){
+        solver[i]=min/(pow(10,i));
+    }
+    
+    //power is running smoothly
+    int j=power-1;
+    for(int i =0;i<power;i++){
+        if(i!=power-1){
+            do{
+                reverse+= (solver[i]-solver[i+1]*10)* pow(10,j);
+            }while(1==2);
+        }
+        else{
+            reverse+=solver[i];
+        }
+        j--;
+    }
+    
+    
+    free(solver);
+    if(x<0){
+        return -reverse;
+    }
+    return reverse;
+}
